259f9b93809e3ac41888aa915f2743d8febd6d8e
Burnsilver <burnsilver@hotmail.com>
2025-05-22 15:02:37 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://mamitavigo.es'
+}));
 app.use(express.json());
 
 const TELEGRAM_TOKEN = '8113673369:AAH9nSwEn_FcNhCYP-DpBuyv21t3tczbjtY';
